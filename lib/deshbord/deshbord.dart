957e84d6bddb3a7a5ef27e2f6a55debba2bbd7fc
Minhaj Mahim <minhaj.thecoder@gmail.com>
2024-06-30 22:04:13 +0600

    Initial commit

---
+++ lib/deshbord/deshbord.dart
@@ -0,0 +1,254 @@
+import 'package:clickaeventsp/screen/main%20manu/budget.dart';
+import 'package:clickaeventsp/screen/main%20manu/calander.dart';
+import 'package:clickaeventsp/screen/main%20manu/checkList.dart';
+import 'package:clickaeventsp/screen/main%20manu/searchBar.dart';
+import 'package:clickaeventsp/screen/main%20manu/sub%20manu/eventSchedulePage.dart';
+import 'package:clickaeventsp/screen/main%20manu/sub%20manu/navbar.dart';
+import 'package:clickaeventsp/screen/widgets/bodyBackground.dart';
+import 'package:clickaeventsp/style/style.dart';
+import 'package:flutter/material.dart';
+
+import '../screen/main manu/event.dart';
+import '../screen/main manu/guestsPage.dart';
+
+class Deshbord extends StatefulWidget {
+  const Deshbord({super.key});
+
+  @override
+  State<Deshbord> createState() => _DeshbordState();
+}
+
+class _DeshbordState extends State<Deshbord> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const Navbar(),
+      appBar: AppBar(
+        backgroundColor: colorRed,
+        centerTitle: true,
+        title: const Text(
+          "clickAEvent",
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                showSearch(context: context, delegate: Search());
+              },
+              icon: const Icon(Icons.search))
+        ],
+      ),
+      body: BodyBackground(
+        child: GridView(
+          padding: const EdgeInsets.all(20),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+            mainAxisExtent: 150,
+          ),
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                  return Calendar();
+                }));
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        "assets/images/calander.png",
+                        width: 50,
+                        height: 50,
+                      ),
+                      const Text(
+                        "Calendar",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                  return const CheckList();
+                }));
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        "assets/images/checklist.png",
+                        width: 50,
+                        height: 50,
+                      ),
+                      const Text(
+                        "Checklist",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                  return BudgetPage();
+                }));
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        "assets/images/budget.png",
+                        width: 60,
+                        height: 60,
+                      ),
+                      const Text(
+                        "Budget",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                  return const Event();
+                }));
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        "assets/images/event.png",
+                        width: 60,
+                        height: 60,
+                      ),
+                      const Text(
+                        "Event",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                  return const EventSchedulePage();
+                }));
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        "assets/images/eventschedule.png",
+                        width: 50,
+                        height: 50,
+                      ),
+                      const Text(
+                        "Event Schedule",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                  return const Guests();
+                }));
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        "assets/images/guests.png",
+                        width: 50,
+                        height: 50,
+                      ),
+                      const Text(
+                        "Guests",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
